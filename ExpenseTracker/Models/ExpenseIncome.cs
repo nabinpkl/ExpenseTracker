14c475b7078ea55f26f7dd70bde3b76bbcbf9fcf
nbn <nabin62246@gmail.com>
2019-09-13 12:04:11 +0545

    Expense model refined

---
+++ ExpenseTracker/Models/ExpenseIncome.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Models
+{
+    public class ExpenseIncome
+    {
+        public int Id { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public string Tag { get; set; }
+
+        [Display(Name = "Entry Date")]
+        [DataType(DataType.Date)]
+        public DateTime EntryDate { get; set; }
+        public bool IsIncome { get; set; }
+
+        public string UserId { get; set; }
+
+        public IdentityUser User { get; set; }
+    }
+}
