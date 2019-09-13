9f40b93b62a97b1bca652f648bc6ac3cb63b654e
nbn <nabin62246@gmail.com>
2019-09-13 12:03:52 +0545

    Added model to database context

---
+++ ExpenseTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ExpenseTracker.Models;
+
+namespace ExpenseTracker.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ExpenseIncome> ExpensesIncomes { get; set; }
+    }
+}
